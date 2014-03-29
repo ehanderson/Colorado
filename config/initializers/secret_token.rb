a6ed7424b25c6baf4991972edf01b28be9c45456
Mac Owner <macowner@Macs-MacBook-Pro.local>
2014-03-29 13:26:25 -0500

    climb higher first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colorado::Application.config.secret_token = '80f218e92883867220091fd7bdd59164a7be8cacf92d55010cd5a8494dd4f3f1dadc8f0c9162188784f4d8611ff2e60ca3f9aa16746ef79a8fd05c7f98482307'
