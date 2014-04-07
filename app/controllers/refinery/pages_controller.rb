3947135ed676dc0bee8a21f9aee3dfca8036e840
Mac Owner <macowner@Macs-MacBook-Pro.local>
2014-04-06 21:18:46 -0500

    editing pages controller to updated version

--- app/controllers/refinery/pages_controller.rb
+++ app/controllers/refinery/pages_controller.rb
@@ -104,13 +104,16 @@ module Refinery
     alias_method :page, :find_page
 
     def render_with_templates?(render_options = {})
-      if Refinery::Pages.use_layout_templates && page.layout_template.present?
-        render_options[:layout] = page.layout_template
-      end
-      if Refinery::Pages.use_layout_templates && page.view_template.present?
-        render_options[:action] = page.view_template
-      end
+
       render render_options if render_options.any?
+    end
+
+    def render_options_for_template(page)
+        render_options = {}
+        template = page.link_url == "/" ? "home" : "show"
+        render_options[:template] = "refinery/pages/#{page.view_template.presence || template}"
+
+        render_options
     end
 
     def set_canonical
