d4c4c95c84ed98f3785158905ab5ca4144835efa
Mac Owner <macowner@Macs-MacBook-Pro.local>
2014-04-08 18:15:19 -0500

    changing editor options

---
+++ app/assets/javascripts/admin.js
@@ -0,0 +1,15 @@
+// Use this to customize the wymeditor boot process
+// Just mirror the options specified in boot_wym.js with the new options here.
+// This will completely override anything specified in boot_wym.js for that key.
+// e.g. skin: 'something_else'
+if (typeof(custom_wymeditor_boot_options) == "undefined") {
+      custom_wymeditor_boot_options = {
+        containersItems: [
+                        {'name': 'H2', 'title': 'Heading_2', 'css': 'wym_containers_h2'},
+                        {'name': 'H3', 'title': 'Heading_3', 'css': 'wym_containers_h3'},
+                        {'name': 'H4', 'title': 'Heading_4', 'css': 'wym_containers_h4'},
+                        {'name': 'BLOCKQUOTE', 'title': 'Blockquote', 'css': 'wym_containers_blockquote'},
+                            {'name': 'P', 'title': 'Paragraph', 'css': 'wym_containers_p'}
+                    ]
+      };
+    }
