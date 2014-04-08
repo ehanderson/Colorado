a46524dcdeecfcf5aabff909b8b7a648941753ad
Mac Owner <macowner@Macs-MacBook-Pro.local>
2014-04-07 19:29:00 -0500

    removing old migration of web users

--- db/migrate/20140406215337_add_phone_to_refinery_web_users.rb
+++
@@ -1,5 +0,0 @@
-class AddPhoneToRefineryWebUsers < ActiveRecord::Migration
-  def change
-    add_column :refinery_web_users, :phone_number, :string
-  end
-end
--- db/migrate/20140406215746_add_story_to_refinery_web_users.rb
+++
@@ -1,5 +0,0 @@
-class AddStoryToRefineryWebUsers < ActiveRecord::Migration
-  def change
-    add_column :refinery_web_users, :story, :text
-  end
-end
