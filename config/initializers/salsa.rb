48f149ae5acc9506518da8febf56995a68b426f7
EmilyAnderson <macowner@EmilyAnderson.local>
2014-05-16 14:45:48 -0500

    allowing for color options in admin

--- config/initializers/salsa.rb
+++ config/initializers/salsa.rb
@@ -2,11 +2,12 @@
 
 response = RestClient.get "https://hq-salsa4.salsalabs.com/api/authenticate.sjs?&email=#{ENV["SALSA_LABS_API_EMAIL"]}.com&password=#{ENV["SALSA_LABS_API_PASSWORD"]}"
 cookie = response.cookies
-t = RestClient.get("https://hq-salsa4.salsalabs.com/api/getObjects.sjs?xml&object=email_blast",
+xml_response = RestClient.get("https://hq-salsa4.salsalabs.com/api/getObjects.sjs?xml&object=email_blast",
                 cookies: cookie
               )
-w = Hash.from_xml(t).to_json
-parsed_response = JSON.parse(w)
+# # # receive xml, convert to json and parse
+json_response = Hash.from_xml(xml_response).to_json
+parsed_response = JSON.parse(json_response)
 blasts = parsed_response['data']['email_blast']['item'].reverse
 $all_blasts = []
 
